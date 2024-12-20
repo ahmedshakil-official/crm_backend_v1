113afdc4c81df9fa86452d1de5b1b90d8aa9eb78
Shakil Ahmed <ahmedshakil.official@gmail.com>
2024-12-20 15:37:37 +0600

    120 days for JWT validity

--- crm_backend_v1/settings.py
+++ crm_backend_v1/settings.py
@@ -242,7 +242,7 @@ DJOSER = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=120),
 }
 
 SPECTACULAR_SETTINGS = {
