f52eb6e965f5b49732e5fc872386e86f32f3da9d
Shakil Ahmed <ahmedshakil.official@gmail.com>
2025-02-06 15:23:57 +0600

    changed port

--- crm_backend_v1/settings.py
+++ crm_backend_v1/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="localhost").split(",")
-# ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="localhost").split(",")
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 
