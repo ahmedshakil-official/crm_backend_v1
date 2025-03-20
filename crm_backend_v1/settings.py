bf348e384e7518a58e77e42cbbbadc01dca54c6d
Ahmed Shakil <ahmedshakil.official@gmail.com>
2025-03-20 12:43:55 +0600

    fixed bug

--- crm_backend_v1/settings.py
+++ crm_backend_v1/settings.py
@@ -234,6 +234,7 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
     ],
+    "COERCE_DECIMAL_TO_STRING": False,
 }
 
 DJOSER = {
