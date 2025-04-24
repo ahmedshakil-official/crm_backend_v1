5bf37110451392fb2aee04e8d9513fb7518b9cac
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2025-04-24 11:41:02 +0600

    create signal for compliance model.

--- case/signals.py
+++ case/signals.py
@@ -172,6 +172,8 @@ def create_suitability(sender, instance, created, **kwargs):
         Suitability = apps.get_model("case", "Suitability")
         Suitability.objects.create(case=instance)
 
+
+
 @receiver(post_save, sender="case.Case")
 def create_compliance(sender, instance, created, **kwargs):
     if created:
