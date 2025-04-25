b97674bf585918435effb024566a71436732aa1c
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2025-04-25 11:55:12 +0600

    Solved model issue. Also solved migrations issue. and marge migrations file.

---
+++ case/migrations/0079_merge_0077s.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('case', '0077_budgetaffordability_budget_affordability_type_and_more'),
+        ('case', '0077_compliance'),
+        ('case', '0077_otherquestion'),
+        ('case', '0078_remove_otherquestion_extra_question'),
+    ]
+
+    operations = [
+    ]
