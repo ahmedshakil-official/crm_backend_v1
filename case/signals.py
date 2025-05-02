0c32cef4a522dd786778c73f7f417d14a362df73
Ahmed Shakil <ahmedshakil.official@gmail.com>
2025-05-02 13:26:43 +0600

    Fixed Bug.

--- case/signals.py
+++ case/signals.py
@@ -212,8 +212,3 @@ def create_compliance(sender, instance, created, **kwargs):
         Compliance.objects.create(case=instance)
 
 
-@receiver(post_save, sender="case.Case")
-def create_mortgage_needs(sender, instance, created, **kwargs):
-    if created:
-        MortgageNeeds = apps.get_model("case", "MortgageNeeds")
-        MortgageNeeds.objects.create(case=instance)
