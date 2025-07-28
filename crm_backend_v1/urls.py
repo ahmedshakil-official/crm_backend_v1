de08c2b916ce25a42d2be0b195985bdf25f1ae85
AshikLikhon <mdarlikhon317@gmail.com>
2025-07-28 16:18:28 +0600

    Create CompletedMortgageCaseStatsView for Mortgage cases Completed. Urls:network/completed-cases/

--- crm_backend_v1/urls.py
+++ crm_backend_v1/urls.py
@@ -26,6 +26,8 @@ from drf_spectacular.views import (
 )
 from rest_framework import permissions
 
+from case.views import CompletedMortgageCaseStatsView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
@@ -34,6 +36,11 @@ urlpatterns = [
     path("director/", include("organizationuser.urls")),
     path("cases/", include("case.urls")),
     path("reports/", include("report.urls")),
+    path(
+        "network/completed-cases/",
+        CompletedMortgageCaseStatsView.as_view(),
+        name="network-completed-cases"
+    ),
 ]
 
 if settings.DEBUG:
