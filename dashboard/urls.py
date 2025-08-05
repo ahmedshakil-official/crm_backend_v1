902781ac0ef3c2af789288ed75560a346fc5d312
Shakil Ahmed <ahmedshakil.official@gmail.com>
2025-08-06 01:03:56 +0600

    Bug fixed.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import OrganizationNetworkDashboardListView
 
 urlpatterns = [
-    path("common/", OrganizationNetworkDashboardListView.as_view(), name='network-dashboard'),
+    path("common/", OrganizationNetworkDashboardListView.as_view(), name='common-dashboard'),
 ]
