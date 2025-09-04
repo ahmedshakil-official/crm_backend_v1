f214b340fa3ed68fbc0353627b72b9d2477c3b0f
AshikLikhon <mdarlikhon317@gmail.com>
2025-09-04 16:19:18 +0600

    change url name.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,5 @@ from .views import OrganizationNetworkDashboardListView, OrganizationDashboardLi
 urlpatterns = [
     path("common/", OrganizationNetworkDashboardListView.as_view(), name='common-dashboard'),
     path("organization/<slug:slug>/", OrganizationDashboardListView.as_view(), name='network-organization-dashboard'),
-    path("network/organisations/<slug:org_slug>/<str:org_user>/", OrganisationStatusView.as_view(), name="org-status"),
+    path("organisations/<slug:org_slug>/<str:org_user>/", OrganisationStatusView.as_view(), name="org-status"),
 ]
